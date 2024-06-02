690fe80a59d1b4d598ec87b6a66b8fce3215a896
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2024-06-02 09:08:03 -0400

    initial

---
+++ lib/mem940.ex
@@ -0,0 +1,18 @@
+defmodule Mem940 do
+  @moduledoc """
+  Documentation for `Mem940`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mem940.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mem940_test.exs
@@ -0,0 +1,8 @@
+defmodule Mem940Test do
+  use ExUnit.Case
+  doctest Mem940
+
+  test "greets the world" do
+    assert Mem940.hello() == :world
+  end
+end
